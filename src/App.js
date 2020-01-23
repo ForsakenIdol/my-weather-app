7001deb181d08cbb86e1651f5b367b11c87921b1
Lachlan D Whang <lachlandwhang@gmail.com>
2020-01-23 10:59:19 +0800

    created column layouts for weather sections and styled with css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="weather-app">
-      <header className="weather-today">
-
+    <html class="weather-app">
+      <header class="weather-today">
+        <div class="headcolumn">
+          {/* These buttons should move the forecast forward or back a day in the week.
+            * Does nothing if user tries to move to yesterday.
+            */}
+          <button onClick={() => {console.log("something")}} class="nav-button">Previous day</button>
+        </div>
+        <div class="headcolumn">
+          <p>-- °C</p> {/* Replace this later with the data from the api */}
+        </div>
+        <div class="headcolumn">
+          <button onClick={() => {console.log("something")}}  class="nav-button">Next day</button>
+        </div>
       </header>
-      <body className="weather-later">
-
+      <body class="weather-later">
+            <div class="bodycolumn">
+              <p>Tomorrow</p>
+              <p>-- °C</p>
+            </div>
+            <div class="bodycolumn">
+              <p>Day 2</p>
+              <p>-- °C</p>
+            </div>
+            <div class="bodycolumn">
+              <p>Day 3</p>
+              <p>-- °C</p>
+            </div>
+            <div class="bodycolumn">
+              <p>Day 4</p>
+              <p>-- °C</p>
+            </div>
+            <div class="bodycolumn">
+              <p>Day 5</p>
+              <p>-- °C</p>
+            </div>
       </body>
-      <footer className="about-app">
+      <footer class="about-app">
         <p>
           This app was made by Lachlan and developed at Takor.
           It is a simple weather app to demonstrate API fetch requests and practise good git commits.
         </p>
       </footer>
-    </div>
+    </html>
   );
 }
 
