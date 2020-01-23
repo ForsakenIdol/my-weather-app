f0ff560fd57d1ac99658304f9a6f8642df27d355
Lachlan D Whang <lachlandwhang@gmail.com>
2020-01-23 13:27:19 +0800

    added relevant data from api to state variables

--- src/App.js
+++ src/App.js
@@ -14,14 +14,39 @@ class App extends React.Component {
     }
   }
 
+  componentDidMount() {
+    this.getWeatherData();
+  }
+
+  kelvinToCelsius = (kelvin) => {return (kelvin - 273.15);}
+
   /* Fetch API address: http://api.openweathermap.org/data/2.5/forecast?id=2063523&APPID=a61002d90fe4eaac824f28012985aa2c */
+  getWeatherData = () => {
+    fetch("http://api.openweathermap.org/data/2.5/forecast?id=2063523&APPID=a61002d90fe4eaac824f28012985aa2c")
+    .then((response) => {
+      if (!response.ok) throw new Error("Weather API response was not ok");
+
+      return response.json();
+    }).then(json_result /* This is the response.json() */ => {
+      this.setState({day1: parseFloat(this.kelvinToCelsius(json_result.list[0].main.temp).toFixed(4))});
+      this.setState({day2: parseFloat(this.kelvinToCelsius(json_result.list[6].main.temp).toFixed(4))});
+      this.setState({day3: parseFloat(this.kelvinToCelsius(json_result.list[12].main.temp).toFixed(4))});
+      this.setState({day4: parseFloat(this.kelvinToCelsius(json_result.list[18].main.temp).toFixed(4))});
+      this.setState({day5: parseFloat(this.kelvinToCelsius(json_result.list[24].main.temp).toFixed(4))});
+    })
+    .catch((error) => {
+      console.log(error);
+      return error;
+    });
+  }
 
   render() {
     return (
       <html class="weather-app">
         <header class="weather-today">
           <div class="headcolumn">
-            {/* This button should move the big forecast back a day in the week.
+            {/*
+              * This button should move the big forecast back a day in the week.
               * Does nothing if user tries to move to yesterday.
               */}
             <button onClick={() => {console.log("something")}} class="nav-button">Previous day</button>
@@ -30,7 +55,8 @@ class App extends React.Component {
             <p>{this.state.day1 == null ? "--" : this.state.day1} °C</p> {/* Replace this later with the data from the api */}
           </div>
           <div class="headcolumn">
-            {/* This button should move the big forecast forward a day in the week.
+            {/*
+              * This button should move the big forecast forward a day in the week.
               * Does nothing if user tries to move past day 5.
               */}
             <button onClick={() => {console.log("something")}}  class="nav-button">Next day</button>
