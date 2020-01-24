2aed286c9438eac6aeca7963aa55153f15bff6a7
Lachlan D Whang <lachlandwhang@gmail.com>
2020-01-24 13:51:41 +0800

    added weather descriptions to forecast

--- src/App.js
+++ src/App.js
@@ -14,6 +14,13 @@ class App extends React.Component {
       day4: null,
       day5: null,
 
+      /* Weather description variables (used for icons) */
+      day1Weather: null,
+      day2Weather: null,
+      day3Weather: null,
+      day4Weather: null,
+      day5Weather: null,
+
       /* Other variables */
       timeInc: 0, /* Navigation for the day */
       totalLines: null,
@@ -53,6 +60,19 @@ class App extends React.Component {
         parseFloat(this.kelvinToCelsius(json_result.list[18 + this.state.timeInc].main.temp).toFixed(0))});
       this.setState({day5: 24 + this.state.timeInc > this.state.totalLines ? null :
         parseFloat(this.kelvinToCelsius(json_result.list[24 + this.state.timeInc].main.temp).toFixed(0))});
+
+      /* Set temperature descriptions for each day; this will be used in conjunction with the weather icons */
+      this.setState({day1Weather: this.state.timeInc > this.state.totalLines ? null :
+        json_result.list[this.state.timeInc].weather[0].main})
+      this.setState({day2Weather: 6 + this.state.timeInc > this.state.totalLines ? null :
+        json_result.list[6 + this.state.timeInc].weather[0].main})
+      this.setState({day3Weather: 12 + this.state.timeInc > this.state.totalLines ? null :
+        json_result.list[12 + this.state.timeInc].weather[0].main})
+      this.setState({day4Weather: 18 + this.state.timeInc > this.state.totalLines ? null :
+         json_result.list[18 + this.state.timeInc].weather[0].main})
+      this.setState({day5Weather: 24 + this.state.timeInc > this.state.totalLines ? null :
+        json_result.list[24 + this.state.timeInc].weather[0].main})
+
     })
     .catch((error) => {
       console.log(error);
@@ -82,81 +102,81 @@ class App extends React.Component {
   render() {
     return (
       <div className="p-3 mb-2 bg-dark text-white">
-        <div class="row" id="title-forecast">
-          <div class="col-md-4">{/* Empty spacer */}</div>
-          <div class="col-md-4">
-            <div class="text-center">
-              <h3>Today's Forecast:</h3>
+        <div className="row" id="title-forecast">
+          <div className="col-md-4">{/* Empty spacer */}</div>
+          <div className="col-md-4">
+            <div className="text-center">
+              <h3>Today's Forecast: {this.state.day1Weather == null ? "--" : this.state.day1Weather}</h3>
               <h5><small>{this.state.city == null ? "undefined" : this.state.city}, 
               {this.state.country == null ? "undefined" : this.state.country}</small></h5>
             </div>
           </div>
-          <div class="col-md-4">{/* Empty spacer */}</div>
+          <div className="col-md-4">{/* Empty spacer */}</div>
         </div>
-        <div class="row" id="current-forecast">
-          <div class="col-md-4">
-            <div class="text-center">
-              <button type="button" onClick={this.decrementTime} class="btn btn-primary btn-sm">Remove 3 hours</button>
+        <div className="row" id="current-forecast">
+          <div className="col-md-4">
+            <div className="text-center">
+              <button type="button" onClick={this.decrementTime} className="btn btn-primary btn-sm">Remove 3 hours</button>
             </div>
           </div>
-          <div class="col-md-4">
-            <div class="text-center"><h1>{this.state.day1 == null ? "--" : this.state.day1} °C</h1></div>
+          <div className="col-md-4">
+            <div className="text-center"><h1>{this.state.day1 == null ? "--" : this.state.day1} °C</h1></div>
           </div>
-          <div class="col-md-4">
-            <div class="text-center">
-              <button type="button" onClick={this.incrementTime} class="btn btn-primary btn-sm">Add 3 hours</button>
+          <div className="col-md-4">
+            <div className="text-center">
+              <button type="button" onClick={this.incrementTime} className="btn btn-primary btn-sm">Add 3 hours</button>
             </div>
           </div>
         </div>
-        <div class="mt-5" id="spacer">{/* Empty row spacer */}</div>
-        <div class="row" id="later-forecast">
-          <div class="col-md-3">
-            <div class="text-center">
-              <p class="lead">Tomorrow</p>
+        <div className="mt-5" id="spacer">{/* Empty row spacer */}</div>
+        <div className="row" id="later-forecast">
+          <div className="col-md-3">
+            <div className="text-center">
+              <p className="lead">Tomorrow: {this.state.day2Weather == null ? "--" : this.state.day2Weather}</p>
               <h2>{this.state.day2 == null ? "--" : this.state.day2} °C</h2>
             </div>
           </div>
-          <div class="col-md-3">
-            <div class="text-center">
-              <p class="lead">Day 3</p>
+          <div className="col-md-3">
+            <div className="text-center">
+              <p className="lead">Day 3: {this.state.day3Weather == null ? "--" : this.state.day3Weather}</p>
               <h2>{this.state.day3 == null ? "--" : this.state.day3} °C</h2>
             </div>
           </div>
-          <div class="col-md-3">
-            <div class="text-center">
-              <p class="lead">Day 4</p>
+          <div className="col-md-3">
+            <div className="text-center">
+              <p className="lead">Day 4: {this.state.day4Weather == null ? "--" : this.state.day4Weather}</p>
               <h2>{this.state.day4 == null ? "--" : this.state.day4} °C</h2>
             </div>
           </div>
-          <div class="col-md-3">
-            <div class="text-center">
-              <p class="lead">Day 5</p>
+          <div className="col-md-3">
+            <div className="text-center">
+              <p className="lead">Day 5: {this.state.day5Weather == null ? "--" : this.state.day5Weather}</p>
               <h2>{this.state.day5 == null ? "--" : this.state.day5} °C</h2>
             </div>
           </div>
         </div>
-        <div class="mt-5" id="spacer">{/* Empty row spacer */}</div>
-        <div class="row" id="reset-button">
-          <div class="col-md-4">{/* Empty spacer */}</div>
-          <div class="col-md-4">
-            <div class="text-center">
-              <button type="button" onClick={() => {this.setState({timeInc: 0}); this.getWeatherData();}} class="btn btn-danger btn-sm">Reset Forecast</button>
+        <div className="mt-5" id="spacer">{/* Empty row spacer */}</div>
+        <div className="row" id="reset-button">
+          <div className="col-md-4">{/* Empty spacer */}</div>
+          <div className="col-md-4">
+            <div className="text-center">
+              <button type="button" onClick={() => {this.setState({timeInc: 0}); this.getWeatherData();}} className="btn btn-danger btn-sm">Reset Forecast</button>
             </div>
           </div>
-          <div class="col-md-4">{/* Empty spacer */}</div>
+          <div className="col-md-4">{/* Empty spacer */}</div>
         </div>
-        <div class="mt-5" id="spacer">{/* Empty row spacer */}</div>
-        <div class="row" id="about-app">
-          <div class="col-md-4">{/* Empty spacer */}</div>
-          <div class="col-md-4">
-            <div class="text-center">
+        <div className="mt-5" id="spacer">{/* Empty row spacer */}</div>
+        <div className="row" id="about-app">
+          <div className="col-md-4">{/* Empty spacer */}</div>
+          <div className="col-md-4">
+            <div className="text-center">
               <h6><small>This app was made by Lachlan and developed at Takor.
                 It is a simple weather app to demonstrate API fetch requests and practise good git commits.</small></h6>
                 <h6><small>Current date and time of forecast: 
                   {this.state.forecastTime == null ? "--" : this.state.forecastTime}</small></h6>
             </div>
           </div>
-          <div class="col-md-4">{/* Empty spacer */}</div>
+          <div className="col-md-4">{/* Empty spacer */}</div>
         </div>
       </div>
     );
